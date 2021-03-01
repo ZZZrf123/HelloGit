2195cae87269d3a951f43ad0bf51a0aa01a24d32
ZZZrf123 <826416222@qq.com>
2021-03-01 16:39:23 +0800

    Update HelloGit.java

--- GitHello/src/main/java/com/zrf/git/HelloGit.java
+++ GitHello/src/main/java/com/zrf/git/HelloGit.java
@@ -3,7 +3,7 @@ package com.zrf.git;
 public class HelloGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git HaHaHa!");
     }
 
     public String test(){
