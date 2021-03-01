1dfa1dd9bf430e919b3467f2c0ead584f7f7c0aa
zrf <826416222@qq.com>
2021-03-01 16:15:50 +0800

    【git测试】第二次提交

--- GitHello/src/main/java/com/zrf/git/HelloGit.java
+++ GitHello/src/main/java/com/zrf/git/HelloGit.java
@@ -5,4 +5,8 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
     }
+
+    public String test(){
+        return "Test Method！";
+    }
 }
